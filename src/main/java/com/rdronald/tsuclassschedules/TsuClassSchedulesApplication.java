95ae72b0b768d03c037f4bc6a9567ac967aa05b3
rDronald <rytik.dron@yandex.ru>
2021-05-27 09:24:35 +0400

    Initial commit

---
+++ src/main/java/com/rdronald/tsuclassschedules/TsuClassSchedulesApplication.java
@@ -0,0 +1,13 @@
+package com.rdronald.tsuclassschedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsuClassSchedulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsuClassSchedulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdronald/tsuclassschedules/TsuClassSchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdronald.tsuclassschedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsuClassSchedulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
